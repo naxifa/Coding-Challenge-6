69dcfb141e2d3e2532daf778ea8feacaf5f52b9b
naxifa <maliha1@usf.edu>
2024-09-22 00:12:59 -0400

    Initialize employees with shift schedules

---
+++ employee_shift_management.js
@@ -0,0 +1,14 @@
+
+// Task 1 - Creating an Employees Array of Employee Objects
+
+const employees = [
+
+    { name: 'John', shifts: [{ day: 'Monday', hours: 8 }, { day: 'Wednesday', hours: 6 }] },
+    { name: 'Sara', shifts: [{ day: 'Tuesday', hours: 5 }, { day: 'Thursday', hours: 7 }] },
+    { name: 'David', shifts: [{ day: 'Monday', hours: 8 }] },
+    { name: 'Emily', shifts: [{ day: 'Friday', hours: 8 }] }
+ ];
+
+ console.log(employees);
+
+ 
