640e77b3efc2e0e4868fd52f99739572c8ec42ab
naxifa <maliha1@usf.edu>
2024-09-22 17:04:20 -0400

    Create listAvailableEmployees function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -38,7 +38,7 @@ if (!employee) {
     console.log(`Employee ${employeeName} not found.`);
 }
 // Looking for already assigned shifts 
-let existingShift = employee.shifts.find(shift => shift.day === day);
+let existingShift = employee.shifts.some(shift => shift.day === day);
 if (existingShift) {
     console.log(`${employeeName} is already assigned a shift on ${day}.`);
 
@@ -49,5 +49,47 @@ if (existingShift) {
 }
 }
 // Assigning extra shift to David
+assignShift("David", "Monday", "4");
 assignShift("David", "Tuesday", "5");
 
+
+
+// Task 4 - Creating a Function to Calculate Total Hours Worked
+
+function calculateTotalHours (employee) {
+
+    // Calculating total hours
+    let totalHours = employee.shifts.reduce((sum,shift) => sum + shift.hours, 0);
+
+    console.log(`${employee.name} - Worked ${totalHours} hours this week`);
+    return totalHours;
+}
+let employee = employees.find(employee => employee.name === "John");  // Calling the function
+if (employee) {
+
+    calculateTotalHours(employee);
+}
+
+
+// Task 5 - Creating a Function to List Employees with Free Days
+
+function listAvailableEmployees (day) {
+
+     // Filtering employees who do not have a shift on the given day
+     let availableEmployees = employees.filter(employee => !employee.shifts.some(shift => shift.day === day)
+    );
+
+    // If the filtered array is empty, log an error message
+    if (availableEmployees.length === 0) {
+        console.log(`No employees available on ${day}`);
+    } else {
+        console.log(`Employees available on ${day}:`);
+        availableEmployees.forEach(employee => {
+             console.log(employee.name);
+        });
+    }
+}
+
+// Calling the function
+listAvailableEmployees("Monday");  
+listAvailableEmployees("Tuesday"); 
